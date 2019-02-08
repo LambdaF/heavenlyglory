4d05744fa2300c1e5ae0dfa40511da3f6b292912
LambdaFunction <datlambdafunction@gmail.com>
2019-02-08 17:29:07 +0000

    Refactored to use asyncio; python 3.7

--- heavenlyglory.py
+++ heavenlyglory.py
@@ -1,47 +1,37 @@
-#!/bin/python3
-import argparse, os, sys, socket, tempfile, shutil, xmltodict
+#!./env/bin/python
+import argparse
+import os
+import ipaddress
+import subprocess
+import asyncio
 
-def performMasscan(flags,interface,targets,tempDir):
-    for t in targets:
-        cmd = "sudo masscan {flags} -i {interface} -oG {tempDir}/mass.scan --append-output {target}".format(flags=flags,interface=interface,tempDir=tempDir,target=t)
-        print("[+] {}".format(cmd))
-        os.system(cmd)
-    return "{}/mass.scan".format(tempDir)
 
-def parseMasscan(scanInput):
-    results = {}
-    with open(scanInput, 'r') as f:
-        for line in f:
-            line = line.strip()
-            splits = line.split()
-            if splits[0] == "#":
-                continue
-            ip = splits[1]
-            ports = []
-            for i in range(4,len(splits)):
-                index = splits[i].index('/')
-                ports.append(splits[i][:index])
-            ports = ','.join(ports)
-            if ip in results:
-                results[ip] += ",{}".format(ports)
-            else:
-                results[ip] = ports
-    return results
+def expandRange(cidr: str) -> list:
+    if cidr[0].isalpha():
+        return [cidr]
+    else:
+        return [str(ip) for ip in ipaddress.IPv4Network(cidr)]
 
-def performNmap(targets, flags, tempDir):
-    for k,v in targets.items():
-            cmd = "sudo nmap {flags} -p {ports} -oA {tempDir}/{ip} {ip}".format(flags=flags,ports=v,tempDir=tempDir,ip=k)
-            print("\n[+] " + cmd)
-            os.system(cmd)
+
+def stripScheme(target: str) -> str:
+    from urllib.parse import urlparse
+    parsed = urlparse(target)
+    return parsed.hostname if parsed.hostname is not None else parsed.path
+
+
+def parseTargets(fileName: str) -> set:
+    targets = set()
+    with open(fileName, 'r') as f:
+        from itertools import chain
+        targets = [expandRange(stripScheme(x.strip()))
+                   for x in f.read().split()]
+        targets = chain.from_iterable(targets)
+    return set(targets)
+
 
 def resolveTarget(target):
-    if target.find("/") > -1:
-        if target[target.find("/") + 1] == '/':
-            print("[-] Attempting to strip URI in {}".format(target))
-            target = target.replace('/','')
-            target = target[target.find(":")+1:]
-        else: #assume it's a range
-            return target
+    import socket
+    import sys
     try:
         result = socket.gethostbyname(target)
     except socket.error:
@@ -49,121 +39,124 @@ def resolveTarget(target):
         sys.exit(1)
     return result
 
-def parseTargets(targetFile):
-    targets = []
-    with open(targetFile, 'r') as f:
-        targets = f.read()
-        if targets.find(',') > - 1:
-            print("[-] Found comma in targets file, assuming comma seperated")
-            targets = targets.split(',')
-        else:
-            targets = targets.split()
-    for i in range(len(targets)):
-        targets[i] = targets[i].strip()
-        targets[i] = resolveTarget(targets[i])
-    return set(targets)
 
-def parseOutput(indir):
-    result = []
-    writeFirst = False
-    for file in os.listdir(indir):
-        if file.endswith(".xml"):
-            tempResult = {}
-            obj = None
-            with open(os.path.join(indir,file), 'r') as f:
-                obj = xmltodict.parse(f.read())
+def parseMasscan(target, result) -> list:
+    final = [target]
+    ports = set()
+    result = str(result).split('\\r')
+    for r in result:
+        if "Discovered" in r:
+            for dis in r.split('\\n'):
+                dis = dis.split()
+                try:
+                    ports.add(dis[3].split('/')[0])
+                except:
+                    pass
+    final.append(ports)
+    return final
 
-            # workaround, because who wants consistent output? thanks xmltodict
-            try:
-                tempResult["ip"] = obj["nmaprun"]["host"]["address"]["@addr"]
-            except:
-                tempResult["ip"] = obj["nmaprun"]["host"]["address"][0]["@addr"]
 
-            tempResult["ports"] = []
-            ports = None
+async def performMasscan(target: str, interface: str, flags: list) -> list:
+    if target[0].isalpha():
+        target = resolveTarget(target)
+    cmd = " ".join(["sudo", "masscan"] + flags.split() +
+                   ["-i", interface, target])
+    print(f"[+] {cmd}")
 
-            # see previous comment
-            try:
-                if isinstance(obj["nmaprun"]["host"]["ports"]["port"], dict):
-                    ports = [obj["nmaprun"]["host"]["ports"]["port"]]
+    proc = await asyncio.create_subprocess_shell(
+        cmd,
+        stdout=asyncio.subprocess.PIPE,
+        stderr=asyncio.subprocess.PIPE)
+
+    result, _ = await proc.communicate()
+
+    return parseMasscan(target, result)
+
+
+def parseNmap(target, result) -> list:
+    final = []
+    result = str(result).split('\\n')
+    for r in result:
+        if "Ports:" in r:
+            r = r.split('\\t')[1]  # ports
+            r = r.split(',')
+            firstRun = True
+            for port in r:
+                port = port.lstrip("Ports: ").split('/')
+                first = ".."
+                if firstRun:
+                    first = target
+                    firstRun = False
+                if "Site" in port[0]:  # edge case for ldap servers
+                    final[-1] += f"/ {port[0]}"
                 else:
-                    ports = obj["nmaprun"]["host"]["ports"]["port"]
-            except:
-                #if all ports are filtered...
-                ports = []
+                    final.append(",".join([first, port[0], port[4], port[6]]))
+    return final
 
-            for port in ports:
-                tempDict = {}
-                tempDict["portNumber"] = port["@portid"]
-                try:
-                    tempDict["serviceName"] = port["service"]["@name"]
-                except:
-                    tempDict["serviceName"] = "unknown"
 
-                try:
-                    tempDict["serviceProduct"] = port["service"]["@product"]
-                    tempDict["serviceVersion"] = port["service"]["@version"]
-                    tempDict["serviceExtra"] = port["service"]["@extrainfo"]
-                except:
-                    pass
-                tempResult["ports"].append(tempDict)
-            result.append(tempResult)
-    return result
+async def performNmap(target: str, ports: list, flags: str) -> dict:
+    cmd = " ".join(["nmap"] + flags.split() +
+                   ["-p", ",".join(ports), "-oG", "-", target])
+    print(f"[+] {cmd}")
 
-def writeCSV(parsed, outfile):
-    with open(outfile, 'w') as f:
-        f.write("Host,Port,Service,Detail\n")
-        for host in parsed:
-            ip = host["ip"]
-            for port in host["ports"]:
-                f.write("{},".format(ip))
-                ip = ".."
-                f.write("{},{},\"".format(port["portNumber"], port["serviceName"]))
-                try:
-                    f.write("{}".format(port["serviceProduct"]))
-                    f.write(" : {}".format(port["serviceVersion"]))
-                    f.write(" : {}".format(port["serviceExtra"]))
-                except:
-                    pass
-                f.write("\"\n")
-    print("[+] Output written to {}".format(outfile))
+    proc = await asyncio.create_subprocess_shell(
+        cmd,
+        stdout=asyncio.subprocess.PIPE,
+        stderr=asyncio.subprocess.PIPE)
+
+    result, _ = await proc.communicate()
+
+    return parseNmap(target, result)
+
+
+async def main(targets, interface, nmapFlags, masscanFlags, outFile):
+    ips = []
+    if os.path.isfile(targets):
+        print("[-] Parsing scope file {}".format(targets))
+        ips = parseTargets(targets)
+    else:
+        ips = expandRange(stripScheme(targets))
+
+    from subprocess import check_output
+    check_output(["sudo", "-v"])  # cache creds
+
+    args = ((ip, interface, masscanFlags) for ip in ips)
+    results = await asyncio.gather(*[performMasscan(*x) for x in args])
+
+    results = await asyncio.gather(*[performNmap(target, ports, nmapFlags)
+                                     for target, ports in results])
+
+    with open(outFile, 'w') as f:
+        for result in results:
+            for line in result:
+                f.write(f"{line}\n")
+
+    print(f"[+] Results written to {outFile}")
+
 
 if __name__ == "__main__":
-    print("It's like a finger pointing away to the moon. Don't concentrate on the finger or you will miss all that heavenly glory.")
+    print("*** It's like a finger pointing away to the moon.")
+    print("Don't concentrate on the finger")
+    print("or you will miss all that heavenly glory. ***\n")
 
-    parser = argparse.ArgumentParser("Passes masscan output to Nmap version scan")
-    parser.add_argument("-t", "--target", required=True, help="Single IP/Hostname/CIDR/Scope file of target(s) to scan")
-    parser.add_argument("-n", "--nmap-flags", default="-Pn -sV -T5 --min-rate 1500", help="Flags for Nmap")
-    parser.add_argument("-m", "--masscan-flags", default="-p1-65535 --rate=20000 --wait 0", help="Flags for masscan")
-    parser.add_argument("-i", "--interface", required=True, help="Network interface to use")
-    parser.add_argument("-o", "--out-file", default="heaven.csv", help="Final result output")
-    parser.add_argument("-k", "--keep-temp", default="", help="Directory to keep temporary scan files in. Files will be removed if not specified")
+    parser = argparse.ArgumentParser(
+        "Performs masscan and passes the output to Nmap version scan")
+    parser.add_argument("-t", "--target", required=True,
+                        help="Single IP/Hostname/CIDR/Scope file of target(s)\
+                             to scan")
+    parser.add_argument("-i", "--interface", required=True,
+                        help="Network interface to use")
+    parser.add_argument(
+        "-n", "--nmap-flags",
+        default="-Pn -sV -T5 --min-rate 1500",
+        help="Flags for Nmap")
+    parser.add_argument("-m", "--masscan-flags",
+                        default="-p1-65535 --rate=20000 --wait=1",
+                        help="Flags for masscan")
+    parser.add_argument("-o", "--out-file",
+                        default="heaven.csv", help="Final result output")
 
     args = parser.parse_args()
 
-    targets = []
-    if os.path.isfile(args.target):
-        print("[-] Parsing scope file {}".format(args.target))
-        targets = parseTargets(args.target)
-    else:
-        targets.append(resolveTarget(args.target))
-
-    with tempfile.TemporaryDirectory() as tempDir:
-        print("[+] Using Temporary Directory {}".format(tempDir))
-
-        print("[+] Performing masscan")
-        resultFile = performMasscan(args.masscan_flags, args.interface, targets, tempDir)
-        results = parseMasscan(resultFile)
-        if not results:
-            print("[!] masscan failed to find any open ports")
-            sys.exit(1)
-
-        print("[+] Performing Nmap")
-        performNmap(results, args.nmap_flags, tempDir)
-
-        parsed = parseOutput(tempDir)
-        writeCSV(parsed, args.out_file)
-
-        if args.keep_temp != "":
-            shutil.copytree(tempDir, args.keep_temp)
-            print("[-] Stored temporary files in {}".format(args.keep_temp))
+    asyncio.run(main(args.target, args.interface, args.nmap_flags,
+                     args.masscan_flags, args.out_file))
