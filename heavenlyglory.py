f1ceaa5e7aabc0da43c7fc6f64fa041bfd474ddc
LambdaFunction <datlambdafunction@gmail.com>
2019-02-11 15:42:00 +0000

    Moved scoped imports to global; added docstrings

--- heavenlyglory.py
+++ heavenlyglory.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python3.7
 import argparse
-import os
 import ipaddress
 import subprocess
 import asyncio
+import os
+import socket
+from urllib.parse import urlparse
+from itertools import chain
+from subprocess import check_output
 
 
 def expandRange(cidr: str) -> list:
+    """ Attempts to expand given CIDR range """
     if cidr[0].isalpha():
         return [cidr]
     else:
@@ -14,33 +19,39 @@ def expandRange(cidr: str) -> list:
 
 
 def stripScheme(target: str) -> str:
-    from urllib.parse import urlparse
+    """ Strips scheme from a given URL """
     parsed = urlparse(target)
     return parsed.hostname if parsed.hostname is not None else parsed.path
 
 
 def parseTargets(fileName: str) -> set:
+    """
+    Parses a file of new line seperated IP addresses/CIDR ranges/URLs
+    Returns a set of results
+    """
     targets = set()
     with open(fileName, 'r') as f:
-        from itertools import chain
         targets = [expandRange(stripScheme(x.strip()))
                    for x in f.read().split()]
         targets = chain.from_iterable(targets)
     return set(targets)
 
 
-def resolveTarget(target):
-    import socket
-    import sys
+def hostnameToIP(target: str) -> str:
+    """ Attempts to reduce a given URL to an IP address """
     try:
         result = socket.gethostbyname(target)
     except socket.error:
         print(f"[!] Error resolving target: {target}")
-        return None
+        return ""
     return result
 
 
-def parseMasscan(target, result) -> list:
+def parseMasscan(target: str, result: str) -> list:
+    """
+    Parses the results of Masscan output
+    Reduces the discovered ports to a list
+    """
     final = [target]
     ports = set()
     result = str(result).split('\\r')
@@ -57,8 +68,13 @@ def parseMasscan(target, result) -> list:
 
 
 async def performMasscan(target: str, interface: str, flags: list) -> list:
+    """
+    Invokes masscan with the given target and arguments
+    Returns a list containing a the target in the first element
+    And a list of open ports in the second
+    """
     if target[0].isalpha():
-        target = resolveTarget(target)
+        target = hostnameToIP(target)
     if not target:
         return [target, []]
     cmd = " ".join(["sudo", "masscan"] + flags.split() +
@@ -75,7 +91,12 @@ async def performMasscan(target: str, interface: str, flags: list) -> list:
     return parseMasscan(target, result)
 
 
-def parseNmap(target, result) -> list:
+def parseNmap(target: str, result: str) -> list:
+    """
+    Parses the results of an nmap scan
+    In to a list formatted for CSV output, in the form:
+    Hostname, port number, service type, service description
+    """
     final = []
     result = str(result).split('\\n')
     for r in result:
@@ -96,7 +117,11 @@ def parseNmap(target, result) -> list:
     return final
 
 
-async def performNmap(target: str, ports: list, flags: str) -> dict:
+async def performNmap(target: str, ports: list, flags: str) -> list:
+    """
+    Invokes nmap with a given list of ports, target and flags
+    Returns a list of results in a format suitable for writing to CSV
+    """
     cmd = " ".join(["nmap"] + flags.split() +
                    ["-p", ",".join(ports), "-oG", "-", target])
     print(f"[+] {cmd}")
@@ -111,7 +136,14 @@ async def performNmap(target: str, ports: list, flags: str) -> dict:
     return parseNmap(target, result)
 
 
-async def main(targets, interface, nmapFlags, masscanFlags, outFile):
+async def main(targets: str, interface: str, nmapFlags: str, masscanFlags: str,
+               outFile: str):
+    """
+    Runs a series of masscans against a given target(s) (synchronous)
+    And then passes the open ports to nmap
+    For detailed service detection (asynchronous);
+    Results are written to the given OutFile in CSV format
+    """
     ips = []
     if os.path.isfile(targets):
         print(f"[-] Parsing scope file '{targets}'")
@@ -119,7 +151,6 @@ async def main(targets, interface, nmapFlags, masscanFlags, outFile):
     else:
         ips = expandRange(stripScheme(targets))
 
-    from subprocess import check_output
     check_output(["sudo", "-v"])  # cache creds
 
     args = ((ip, interface, masscanFlags) for ip in ips)
@@ -147,25 +178,31 @@ async def main(targets, interface, nmapFlags, masscanFlags, outFile):
 
 if __name__ == "__main__":
     print("*** It's like a finger pointing away to the moon.")
-    print("Don't concentrate on the finger")
-    print("or you will miss all that heavenly glory. ***\n")
+    print("*** Don't concentrate on the finger")
+    print("*** Or you will miss all that heavenly glory.\n")
 
     parser = argparse.ArgumentParser(
         "Performs masscan and passes the output to Nmap version scan")
-    parser.add_argument("-t", "--target", required=True,
-                        help="Single IP/Hostname/CIDR/Scope file of target(s)\
-                             to scan")
-    parser.add_argument("-i", "--interface", required=True,
-                        help="Network interface to use")
+    parser.add_argument(
+        "-t", "--target",
+        required=True,
+        help="Single IP/Hostname/CIDR/Scope file of target(s) to scan")
+    parser.add_argument(
+        "-i", "--interface",
+        required=True,
+        help="Network interface to use")
     parser.add_argument(
         "-n", "--nmap-flags",
         default="-Pn -sV -T5 --min-rate 1500",
         help="Flags for Nmap")
-    parser.add_argument("-m", "--masscan-flags",
-                        default="-p1-65535 --rate=20000 --wait=1",
-                        help="Flags for masscan")
-    parser.add_argument("-o", "--out-file",
-                        default="heaven.csv", help="Final result output")
+    parser.add_argument(
+        "-m", "--masscan-flags",
+        default="-p1-65535 --rate=20000 --wait=1",
+        help="Flags for masscan")
+    parser.add_argument(
+        "-o", "--out-file",
+        default="heaven.csv",
+        help="Final result output")
 
     args = parser.parse_args()
 
